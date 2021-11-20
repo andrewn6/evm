141cb6b67b44f33e8621f94d3746ec72d18fbcfe
Andrew <andrewnijmeh1@gmail.com>
2021-11-19 21:42:06 -0500

    update disassembler.py write output
    Signed-off-by: Andrew <andrewnijmeh1@gmail.com>

--- evm/disassembler.py
+++ evm/disassembler.py
@@ -1,9 +1,17 @@
 import sys
+from evm import EVM
 # import convert_bytecode
 
 def get_list_with_prefix(prefix, len):
     return ["{}{}".format(prefix, i) for i in range(len)]
 
 if __name__ == "__main__":
-    pass
+    evm = EVM(bytes.fromhex(input('>> ')))
+    insts, func_list, blocks = evm.disassemble()
+    
+    with open("output", "w") as output:
+        output.write("FUNCTIONS:\n")
+        for addr, infpo in sorted(func_list.items()):
+            output.write('test')
+
 
