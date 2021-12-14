2d3baa7869a633ea71f5fd819e8c75073269e139
Andrew <andrewnijmeh1@gmail.com>
2021-12-14 17:12:40 -0500

    update

--- evm/disassembler.py
+++ evm/disassembler.py
@@ -1,42 +1,38 @@
-import sys
-import os
-# import capstone
 from evm import EVM
+# import capstone
 # import convert_bytecode
 
 def get_list_with_prefix(prefix, len):
     return ['{}{}'.format(prefix, i) for i in range(len)]
 
-if __name__ == "__main__":
-    evm = EVM(bytes.fromhex(input('>> Enter hex: ')))
+
+if __name__ == '__main__':
+    evm = EVM(bytes.fromhex(input('>> ')))
     insts, func_list, blocks = evm.disassemble()
-    
-    with open("output", "w") as output:
-        output.write("FUNCTIONS:\n")
+
+    with open('output', 'w') as output:
+        output.write('FUNCTIONS:\n\n')
         for addr, info in sorted(func_list.items()):
             output.write(
-                    "   FUNC_{:04X}({}) -> ({})\n"
-                    .format(
-                        addr,
-                        ', '.join(get_list_with_prefix('arg', info[0])),
-                        '. '.join(get_list_with_prefix('r', info[1])),
-                    )
+                '  FUNC_{:04X}({}) -> ({})\n'
+                .format(
+                    addr,
+                    ', '.join(get_list_with_prefix('arg', info[0])),
+                    ', '.join(get_list_with_prefix('r', info[1])),
+                )
             )
 
         output.write(
-            '\n--\n'
-            "RESULT:"
+            '\n---\n'
+            'DISASSEMBLED RESULT:'
         )
-
         for addr, visited in sorted(insts.items()):
             if addr in blocks:
                 output.write('\n\nLABEL_{:04X}:'.format(addr))
-                
                 for xref in blocks[addr]:
                     if xref[0] is not None:
-                        output.write("\n {}".format(xref[0]))
-                        
+                        output.write('\n  {}'.format(xref[0]))
                         if xref[1] is not None:
-                            output.write(", if {}".format(xref[1]))
+                            output.write(', if {}'.format(xref[1]))
 
-            output.write("\n 0x{:04X}: []".format(addr, visited[0]))
+            output.write('\n  0x{:04X}: {}'.format(addr, visited[0]))
