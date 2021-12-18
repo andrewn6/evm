4ec4439f8fa1e4749e5d03e70e37d252c646c60c
Andrew <andrewnijmeh1@gmail.com>
2021-12-18 13:51:35 -0500

    mv vagrant to test folder for evm tests + update nix flake for python app

--- flake.nix
+++ flake.nix
@@ -1,11 +1,84 @@
 {
-  description = "A very basic flake";
+  description = "EVM Disassmbler";
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    mach-nix = {
+      url = "github:DavHau/mach-nix/3.3.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.pypi-deps-db.follows = "pypi-deps-db";
+    };
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
+    pypi-deps-db = {
+      url = "github:DavHau/mach-nix/3.3.0";
+    };
   };
+  outputs = { self, nixpkgs, flake-utils, mach-nix, pypi-deps-db }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        python = "python39";
+
+        inherit (nixpkgs.lib) concatStringsSep;
+
+        pkgs = import nixpkgs { inherit system; };
+        mach = import mach-nix { inherit pkgs python; };
+
+        devRequirements = ''
+          black
+          flake8
+          isort
+          mypy
+          pytest
+        '';
+
+        prodRequirements = ''
+          click
+          pip
+        '';
+
+        freeze = ''
+          mkdir -p $out
+          export out_file=$out/requirements.txt
+          pip list --format=freeze > $out/requirements.txt
+        '';
+
+        devPython = mach.mkPython {
+          requirements = concatStringsSep "\n" [
+            prodRequirements
+            devRequirements
+          ];
+        };
+
+        prodPython = mach.mkPython { requirements = prodRequirements; };
+      in
+      rec {
+        packages.prod = mach.buildPythonPackage {
+          src = ./.;
+          requirements = prodRequirements;
+        };
+
+        defaultPackage = packages.prod;
+
+        packages.devRequirements = pkgs.runCommand "dev_requirements"
+          {
+            buildInputs = [ devPython ];
+          }
+          freeze;
+
+        packages.prodRequirements = pkgs.runCommand "prod_requirements"
+          {
+            buildInputs = [ prodPython ];
+          }
+          freeze;
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            fd
+            pyright
+          ] ++ [ devPython ];
+        };
+      });
 }
